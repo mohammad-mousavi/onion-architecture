36339f6466b11437389f44d7aa025af5d754b434
Mohammad Mousavi <m.mousavi@tourismit.ir>
2021-05-31 17:38:35 +0430

    Initial commit

---
+++ endpoints/web-ui/src/test/java/com/tit/digitalbanking/webui/WebUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tit.digitalbanking.webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
